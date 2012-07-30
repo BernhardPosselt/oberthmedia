f423cc9c8e7d8cc3ca97ec2a215a7dfc04aa634d
Bernhard Posselt <nukeawhale@gmail.com>
2012-07-30 20:15:13 +0200

    references that have been uploaded show up on the front page

--- oberthmedia/homepage/models.py
+++ oberthmedia/homepage/models.py
@@ -9,42 +9,42 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class Contact(models.Model):
-    name = models.CharField(_("Name"), max_length=200, help_text=_("Wie heißen \
+    name = models.CharField(_(u"Name"), max_length=200, help_text=_(u"Wie heißen \
         Sie und/oder Ihre Firma?"))
-    phone = models.CharField(_("Telefon"), max_length=200, blank=True,
-        help_text=_("Unter welcher Telefon-Nummer..."))
-    phonetime = models.CharField(_("Anruf möglich um"), max_length=200,
-        blank=True, help_text=_("Wann wollen Sie angerufen werden?"))
-    email = models.EmailField(_("E-Mail"), max_length=200, help_text=_("...\
+    phone = models.CharField(_(u"Telefon"), max_length=200, blank=True,
+        help_text=_(u"Unter welcher Telefon-Nummer..."))
+    phonetime = models.CharField(_(u"Anruf möglich um"), max_length=200,
+        blank=True, help_text=_(u"Wann wollen Sie angerufen werden?"))
+    email = models.EmailField(_(u"E-Mail"), max_length=200, help_text=_("...\
         oder welcher E-Mail Adresse dürfen wir Ihnen antworten?"))
-    message = models.TextField(_("Nachricht"), help_text=_("Was haben Sie für \
+    message = models.TextField(_(u"Nachricht"), help_text=_(u"Was haben Sie für \
         Wünsche?"))
-    timestamp = models.DateTimeField(_("Sendedatum"), auto_now_add=True)
+    timestamp = models.DateTimeField(_(u"Sendedatum"), auto_now_add=True)
 
     def __unicode__(self):
         return self.name
 
     class Meta:
-        verbose_name = _("Kontaktanfrage")
-        verbose_name_plural = _("Kontaktanfragen")
+        verbose_name = _(u"Kontaktanfrage")
+        verbose_name_plural = _(u"Kontaktanfragen")
         ordering = ['-timestamp']
 
 
 class Reference(models.Model):
-    title = models.CharField(_("Titel"), max_length=200)
-    description = models.TextField(_("Kurzbeschreibung"),
-        help_text=_("Zusammenfassender Einleitungstext, der über der Gallerie \
+    title = models.CharField(_(u"Titel"), max_length=200)
+    description = models.TextField(_(u"Kurzbeschreibung"),
+        help_text=_(u"Zusammenfassender Einleitungstext, der über der Gallerie \
             angezeigt wird"))
-    cover = models.ImageField(_("Vorschaubild"), help_text=_("Das kleine Bild, \
-        welches in der Übersicht auftaucht"), upload_to="references")
-    timestamp = models.DateTimeField(_("Erstelldatum"), auto_now_add=True)
-    active = models.BooleanField(_("Öffentlich"), help_text=_("Wenn gesetzt \
+    cover = models.ImageField(_(u"Vorschaubild"), help_text=_(u"Das kleine Bild, \
+        welches in der Übersicht auftaucht (334px breit, 360px hoch)"), upload_to="references")
+    timestamp = models.DateTimeField(_(u"Erstelldatum"), auto_now_add=True)
+    active = models.BooleanField(_(u"Öffentlich"), help_text=_(u"Wenn gesetzt \
         erscheint der Eintrag in der Referenzgallerie"), default=True)
 
     def __unicode__(self):
         return self.title
 
     class Meta:
-        verbose_name = _("Referenz")
-        verbose_name_plural = _("Referenzen")
+        verbose_name = _(u"Referenz")
+        verbose_name_plural = _(u"Referenzen")
         ordering = ['-timestamp']
--- oberthmedia/settings.py
+++ oberthmedia/settings.py
@@ -79,15 +79,6 @@ PROJECT_URL = ''
 # the url which redirects to the login view
 LOGIN_URL = PROJECT_URL + '/login'
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_PATH, 'upload/')
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = PROJECT_URL + '/upload/'
-
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
@@ -97,6 +88,15 @@ STATIC_ROOT = os.path.join(PROJECT_PATH, 'static/')
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = PROJECT_URL + '/static/'
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = os.path.join(STATIC_ROOT, 'upload/')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = PROJECT_URL + '/upload/'
 
 # Additional locations of static files
 # to compile all static files in production run python manage.py collectstatic
