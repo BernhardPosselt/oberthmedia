43bff6cd2117cd7ab15f10c085f376d3fac8c15c
Bernhard Posselt <nukeawhale@gmail.com>
2012-07-31 02:34:13 +0200

    added twitter fetching

--- oberthmedia/homepage/static/homepage/script/main.js
+++ oberthmedia/homepage/static/homepage/script/main.js
@@ -13,19 +13,32 @@ $(document).ready(function(){
      * Switch to night mode when between 22:00 and 6:00 o clock
      */
     var hours = new Date().getHours();
-    if(hours >= 22  || hours <= 6){
+    if(hours >= 20  || hours <= 7){
         toggle_night();
     }
 
     /**
      * Twitter
      */
-    var twitterUser = "OberthMedia";
-    var twitterUrl = "http://search.twitter.com/" + twitterUser + ".json&callback=?&count=1" ;
-    
-    $.getJSON(twitterUrl, function(json){
-        $("#twitter").html(json.followers_count);
-    });
+    var user = 'OberthMedia';
+      
+    // using jquery built in get json method with twitter api, return only one result
+    $.getJSON('http://twitter.com/statuses/user_timeline.json?screen_name=' + user + '&count=1&callback=?', function(data)      {
+          
+        // result returned
+        var tweet = data[0].text;
+      
+        // process links and reply
+        tweet = tweet.replace(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig, function(url) {
+            return '<a href="'+url+'">'+url+'</a>';
+        }).replace(/B@([_a-z0-9]+)/ig, function(reply) {
+            return  reply.charAt(0)+'<a href="http://twitter.com/'+reply.substring(1)+'">'+reply.substring(1)+'</a>';
+        });
+      
+        // output the result
+        $("#twitter .descr").html(tweet);
+    }); 
+
     $("#twitter").click(function(){
         toggle_night();
     });
